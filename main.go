dab87fd477afc449e4f586113c8e63130528fff4
wtc <kxcr777@qq.com>
2025-10-07 20:34:09 +0800

    环境准备与第一个gin应用

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		fmt.Println("我是" + action)
+		//截取/
+		action = strings.Trim(action, "/")
+		c.String(http.StatusOK, name+" is "+action)
+	})
+	r.Run(":8080")
+}
